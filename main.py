dc8a7b371b19825ec42e6a40b023a64448f80688
William Edmiston <wedmiston31@gmail.com>
2022-05-29 20:27:24 +0000

    revert back to initial setup

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-import pandas as pd
-import sklearn
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import KFold, cross_val_score
-
 from flask import Flask
 
 app = Flask(__name__)
