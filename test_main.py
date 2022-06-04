e0b5611a1490956a906c1627c51f3ac69d143596
William Edmiston <wedmiston31@gmail.com>
2022-06-04 00:09:17 +0000

    fixing test

--- test_main.py
+++ test_main.py
@@ -1,8 +1,5 @@
 from main import app
-from hello import add
 
 #Temp comment till I get file set up how I want
-#def test_main():
-#    response = app.test_client().get("/")
-def test_add():
-    assert 2 == add(1,1)
+def test_main():
+   response = app.test_client().get("/")
