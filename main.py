47ad756d5120e72bb6a63d0a0c05a8cb0aae02fa
William Edmiston <wedmiston31@gmail.com>
2022-06-05 22:44:11 +0000

    final update

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
+#from flask import Flask
+
 import dash
 from dash import dash, dcc, html
 from google.cloud import bigquery
 import plotly.express as px
 
+#@app = Flask(__name__)
 app = dash.Dash(__name__)
 server= app.server
+
+
+#@app.route("/")
+#def hello_world():
+    
 client = bigquery.Client()
-
 query = """
-    SELECT * FROM
-    FROM ML.FORECAST(MODEL covid19.Covid_model,
-                     STRUCT(14 AS horizon, 0.9 AS confidence_level))
+SELECT *
+FROM ML.FORECAST(MODEL covid19.Covid_model,
+                    STRUCT(14 AS horizon, 0.9 AS confidence_level))
 """
-
 query_job = client.query(query)
-
 df = query_job.to_dataframe()
+fig = px.line(df, x='forecast_timestamp', y = 'forecast_value')
 
-fig = px.line(df, x='TIME_SERIES_TIMESTAMP_COL', y = 'TIME_SERIES_DATA_COL')
+response = "Test Values2"
+
+#return response
 
 app.layout = html.Div(children = [
     html.H1("Covid-19 cases forecast"),
@@ -30,16 +38,3 @@ app.layout = html.Div(children = [
 
 if __name__ == '__main__':
     app.run_server(debug=True, host="0.0.0.0", port=8080)
-
-#Modified from other projects
-
-#from flask import Flask
-#import requests
-#info = 'https://us-central1-prediction-aiplatform.googleapis.com/v1alpha1/projects/msds434-gcp/locations/us-central1/endpoints/2857701089334001664'
-#data = {'PROJECT_ID':'msds434-gcp', 'REGION':'us-central1', 'ENDPOINT_ID':'2857701089334001664', 'INPUT_DATA_FILE':'INPUT-JSON'}
-#app = Flask(__name__)
-
-#@app.route("/")
-#def hello_world():
-#   response = requests.post(info, data)
-#   return response
